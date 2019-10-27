ca7fdd5742aae4851faa14443b08f2b0d8281c69
Lauren <lauren.c4231@gmail.com>
2019-10-27 23:55:49 +0000

    Feat: Add scaffolding and styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        <div className="App">
+            <div className="form-field">
+                <label>
+                    <div className="label">Please type in your address!</div>
+                    <div data-children-count="1">
+                        <input
+                            autoComplete="off"
+                            autoCorrect="off"
+                            autoCapitalize="none"
+                            spellCheck="false"
+                            onBlur={props.handleBlur}
+                            name="Name"
+                            type="text"
+                        />
+                    </div>
+                </label>
+            </div>
+        </div>
+    );
 }
 
 export default App;
