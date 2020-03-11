9f89cd44343555d6dda715a3e432652e117af763
Lauren <lauren.c4231@gmail.com>
2020-03-11 20:02:06 +0000

    Import google places component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React from "react";
-import "./App.scss";
+import React from 'react';
+import GooglePlaces from './Google-Places-Form';
+import './App.scss';
 
 function App(props) {
     return (
         <div className="App">
-            <div className="form-field">
-                <label>
-                    <div className="label">Please type in your address!</div>
-                    <div data-children-count="1">
-                        <input
-                            autoComplete="off"
-                            autoCorrect="off"
-                            autoCapitalize="none"
-                            spellCheck="false"
-                            onBlur={props.handleBlur}
-                            name="Name"
-                            type="text"
-                        />
-                    </div>
-                </label>
-            </div>
+            <GooglePlaces />
         </div>
     );
 }
