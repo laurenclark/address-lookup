be274931c0af29e3f95ccd0d5daaa1fbc8097858
Lauren <lauren.c4231@gmail.com>
2020-03-14 02:16:22 +0000

    Edit styles

--- src/Google-Places-Form.js
+++ src/Google-Places-Form.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import FloatingInput from './Floating-Input';
 
-export default function GooglePlaces(props) {
+export default function GooglePlaces() {
     return (
-        <FloatingInput
-            placeholder="94 Road Street"
-            labeltext="Address"
-            autoComplete="off"
-            autoCorrect="off"
-            autoCapitalize="none"
-            spellCheck="false"
-            name="address"
-            type="text"
-        />
+        <form>
+            <FloatingInput
+                placeholder="Jane Bloggs"
+                labeltext="Full Name"
+                autoComplete="off"
+                autoCorrect="off"
+                autoCapitalize="none"
+                spellCheck="false"
+                name="name"
+                type="text"
+            />
+            <FloatingInput
+                placeholder="94 Road Street"
+                labeltext="Address"
+                autoComplete="off"
+                autoCorrect="off"
+                autoCapitalize="none"
+                spellCheck="false"
+                name="address"
+                type="text"
+            />
+        </form>
     );
 }
